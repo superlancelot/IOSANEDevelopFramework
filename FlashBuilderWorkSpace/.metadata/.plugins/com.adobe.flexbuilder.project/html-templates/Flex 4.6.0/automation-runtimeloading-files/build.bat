21a940e7d749bbf8273e782db7ae0a7b75225285
labuser <superlancelot@yahoo.cn>
2012-09-05 14:23:54 +0800

    delete worspace

--- FlashBuilderWorkSpace/.metadata/.plugins/com.adobe.flexbuilder.project/html-templates/Flex 4.6.0/automation-runtimeloading-files/build.bat
+++
@@ -1,3 +0,0 @@
-SET OPTS=-include-libraries+=..\..\frameworks\libs\automation\automation.swc;..\..\frameworks\libs\automation\automation_agent.swc;..\..\frameworks\libs\automation\automation_dmv.swc;..\..\frameworks\libs\automation\qtp.swc;..\..\frameworks\libs\automation\automation_spark.swc;..\..\frameworks\libs\automation\automation_flashflexkit.swc;
-
-..\..\bin\mxmlc.exe %OPTS% runtimeloading.mxml
